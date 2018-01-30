d752012082d13fdd772abdf64a4829ef496b50f6
Softhink-Odoo <34195841+Softhink-Odoo@users.noreply.github.com>
2018-01-29 22:57:16 -0600

    Update __manifest__.py

--- catalogos/__manifest__.py
+++ catalogos/__manifest__.py
@@ -3,15 +3,14 @@
     'name': "catalogos",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Contiene los Catalogos requeridos por el SAT para la facturación en 3.3""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Softhink",
+    'website': "http://www.sft.com.mx",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
